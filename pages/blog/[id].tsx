a418cae7da47aaa005a0ef1e2c5594ea41184923
萬健太郎 <k-yorozu@tribeat.com>
2023-12-22 20:33:24 +0900

    css適用

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,4 +1,5 @@
 import { client } from "../../libs/client";
+import styles from "../../styles/Home.module.scss";
 
 type Blog = {
   id: string;
@@ -12,13 +13,14 @@ type Blog = {
 
 export default function BlogId({ blog }: { blog: Blog }) {
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
+        className={styles.post}
       />
     </main>
   );
