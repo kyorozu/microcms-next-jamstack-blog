51952797db7cca991c106003da8110f8f8889c19
萬健太郎 <k-yorozu@tribeat.com>
2023-12-25 20:35:16 +0900

    カテゴリー表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,45 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({ blog }: { blog: Array<any> }) {
-  console.log(blog);
+type Blog = {
+  id: string;
+  title: string;
+  body: string;
+  category: {
+    id: string,
+    name: string,
+    createdAt: string,
+    updatedAt: string,
+    publishedAt: string,
+    revisedAt: string,
+  }
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+}
+
+type Category = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+}
+
+export default function Home({ blog, category }: { blog: Blog[], category: Category[] }) {
+  console.log('blog: ', blog);
+  console.log('category: ', category);
   return (
     <div>
+      <ul>
+        {category.map((category) => (
+          <li key={category.id}>
+            <Link href={`/category/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -20,9 +55,13 @@ export default function Home({ blog }: { blog: Array<any> }) {
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
 
+  // カテゴリーコンテンツの取得
+  const categoryData = await client.get({ endpoint: "categories" });
+
   return {
     props: {
       blog: data.contents,
+      category: categoryData.contents
     },
   };
 };
