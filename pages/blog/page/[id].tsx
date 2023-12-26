000b36b52b0badef61c1cf9ce6e7d223b2657595
萬健太郎 <k-yorozu@tribeat.com>
2023-12-26 13:48:19 +0900

    個別のページネーション

---
+++ pages/blog/page/[id].tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { Pagination } from '../../../components/Pagination';
+import { client } from "../../../libs/client";
+import { Blog } from '@/pages/types';
+
+const PER_PAGE = 5;
+
+export default function BlogPageId({ blog, totalCount }: { blog: Blog[], totalCount: number }) {
+  return (
+    <div>
+      <ul>
+        {blog.map(blog => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Pagination totalCount={totalCount} />
+    </div>
+  );
+}
+
+// 動的なページを作成
+export const getStaticPaths = async () => {
+  const repos = await client.get({ endpoint: "blog" });
+
+  const range = (start: number, end: number) => [...Array(end - start + 1)].map((_, i) => start + i);
+
+  const paths = range(1, Math.ceil(repos.totalCount / PER_PAGE)).map((repo) => `/blog/page/${repo}`);
+
+  return { paths, fallback: false };
+};
+
+// データを取得
+export const getStaticProps = async (context: { params: { id: number }; }) => {
+  const id = context.params.id;
+
+  const data = await client.get({ endpoint: "blog", queries: { offset: (id - 1) * 5, limit: 5 } });
+
+  return {
+    props: {
+      blog: data.contents,
+      totalCount: data.totalCount,
+    },
+  };
+};
